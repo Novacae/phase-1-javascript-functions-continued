a00a0afc0893864948d89657d3ffd84d740c5be7
aman githinji <amangithinji09@gmail.com>
2023-08-03 02:51:53 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // code your solution here
+//function saturdayFun(toDo = "roller-skate!") {
+    //console.log('This Saturday, I want to ${toDo}');
+
+    function saturdayFun( toDo = "roller-skate",b) {
+        return(`This Saturday, I want to ${toDo}!`);
+    }
+
+   function mondayWork(Act = "go to the office",c) {
+   return(`This Monday, I will ${Act}.`)
+   }
+
+   function wrapAdjective(flair= "*")  {
+    return function (adjective = "a hard worker") {
+         (`You are ${flair}${adjective}${flair}!`)
+    }
+     
+}
+    
+  const encouragingPromptFunction =function wrapAdjective(dedd = "||"){
+  console.log(encouragingPromptFunction(`${dedd}a dedicated programmer${dedd}`));}
